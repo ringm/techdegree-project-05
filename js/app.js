e2f76d744b6266e13afd78dcb452350dabe5eb2f
Martin Ring <ring.martin@gmail.com>
2019-07-10 10:54:27 -0300

    added search functionality and improved markup

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 
-lightbox.option({
-  alwaysShowNavOnTouchDevices :	true,
-  showImageNumberLabel : false,
-  imageFadeDuration : 450,
-  maxWidth : 1200,
-  maxHeight : 750,
-  positionFromTop: 150,
-  fadeDuration : 350
+$('[data-fancybox="roadtrip"]').fancybox({
+	// Options will go here
+  infobar: false,
+  smallBtn: false,
+  transitionDuration: 500,
+  toolbar: false
 });
 
-$(document).ready(function() {
-	$('#search').hideseek({
-    attribute: 'data-title',
-    nodata: 'Sorry, no results matching your search.',
-    min_chars: 2
-  });
+$('#search').keyup(function(){
+  let input = $('#search').val().toLowerCase();
+  let searchList = document.querySelectorAll("li");
+
+  for(let i=0; i < searchList.length; i++){
+    let keyword = searchList[i].childNodes[1].getAttribute("data-caption").toLowerCase();
+    if(keyword.indexOf(input) == -1) {
+      searchList[i].classList.add("hide");
+    }
+  }
 });
