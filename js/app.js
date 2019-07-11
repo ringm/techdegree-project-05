27a38d3d7c527eaf4a8957bb2bb5ad0a31dd6686
Martin Ring <ring.martin@gmail.com>
2019-07-11 10:40:49 -0300

    final tune before review

--- js/app.js
+++ js/app.js
@@ -1,20 +1,66 @@
+//--------------------------------------------
+//FANCYBOX
+//--------------------------------------------
 
-$('[data-fancybox="roadtrip"]').fancybox({
-	// Options will go here
-  infobar: false,
-  smallBtn: false,
-  transitionDuration: 500,
-  toolbar: false
-});
+function fancybox() {
+    $('[data-fancybox="roadtrip"]').fancybox({
+    // Options will go here
+    infobar: true,
+    smallBtn: true,
+    transitionDuration: 500,
+    preventCaptionOverlap: false,
+    toolbar: false
+  });
+}
 
-$('#search').keyup(function(){
-  let input = $('#search').val().toLowerCase();
-  let searchList = document.querySelectorAll("li");
+//call it on page load in case user doesn't use the search bar
+fancybox();
 
-  for(let i=0; i < searchList.length; i++){
+//--------------------------------------------
+//SEARCH FILTER
+//--------------------------------------------
+
+//stores search input
+let input = document.getElementById("search");
+//listen for any changes in the input field
+input.addEventListener("input", function() {
+
+  //stores value input from user
+  let inputValue = input.value.toLowerCase();
+  //stores all 'li' elements in the gallery
+  let searchList = document.getElementById("photo-gallery").children;
+  //stores 'p' element with 'no results' msg
+  let noResultMsg = document.getElementById('no-results');
+  //counts how many elements are being hidden
+  let k = 0;
+
+
+  for(let i=0; i < searchList.length; i++) {
+    //stores the img caption value that we need to compare against the user input
     let keyword = searchList[i].childNodes[1].getAttribute("data-caption").toLowerCase();
-    if(keyword.indexOf(input) == -1) {
+
+    //compares caption against user input
+    if(keyword.indexOf(inputValue) == -1) {
+      //if there's no match, hides the element
       searchList[i].classList.add("hide");
+      //also, removes 'data-fancybox' attribute so the img is removed from the lightbox gallery
+      searchList[i].childNodes[1].removeAttribute("data-fancybox");
+      //adds 1 to the counter
+      k++;
+    } else {
+      //removes the 'hide' class so the element shows
+      searchList[i].classList.remove("hide");
+      //set attribute 'data-fancybox' to show again in the lightbox gallery
+      searchList[i].childNodes[1].setAttribute("data-fancybox", "roadtrip");
     }
   }
+
+  //if all the elements are hidden, the 'no results' msg shows
+  if(k == searchList.length) {
+    noResultMsg.classList.remove("hide");
+  } else { noResultMsg.classList.add("hide"); }
+
+  //I call the fancybox so it builds the gallery again based only on the filtered results
+  fancybox();
+
 });
