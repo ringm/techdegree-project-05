81f5cb5276600b6b7a415aa38094a71f82b0645b
Martin Ring <ring.martin@gmail.com>
2019-07-12 10:34:13 -0300

    improved jd & removed unnecesary jquery plugin

--- js/app.js
+++ js/app.js
@@ -22,36 +22,36 @@ fancybox();
 
 //stores search input
 let input = document.getElementById("search");
+//stores all 'li' elements in the gallery
+let searchList = document.querySelectorAll("#photo-gallery li");
+//select all elements with attr 'data-caption'
+let keyword = document.querySelectorAll("[data-caption]");
+//stores 'p' element with 'no results' msg
+let noResultMsg = document.getElementById("no-results");
+
 //listen for any changes in the input field
 input.addEventListener("input", function() {
 
   //stores value input from user
   let inputValue = input.value.toLowerCase();
-  //stores all 'li' elements in the gallery
-  let searchList = document.getElementById("photo-gallery").children;
-  //stores 'p' element with 'no results' msg
-  let noResultMsg = document.getElementById('no-results');
   //counts how many elements are being hidden
   let k = 0;
 
-
   for(let i=0; i < searchList.length; i++) {
-    //stores the img caption value that we need to compare against the user input
-    let keyword = searchList[i].childNodes[1].getAttribute("data-caption").toLowerCase();
-
+    let caption = keyword[i].getAttribute("data-caption");
     //compares caption against user input
-    if(keyword.indexOf(inputValue) == -1) {
+    if(caption.indexOf(inputValue) == -1) {
       //if there's no match, hides the element
       searchList[i].classList.add("hide");
       //also, removes 'data-fancybox' attribute so the img is removed from the lightbox gallery
-      searchList[i].childNodes[1].removeAttribute("data-fancybox");
+      keyword[i].removeAttribute("data-fancybox");
       //adds 1 to the counter
       k++;
     } else {
       //removes the 'hide' class so the element shows
       searchList[i].classList.remove("hide");
       //set attribute 'data-fancybox' to show again in the lightbox gallery
-      searchList[i].childNodes[1].setAttribute("data-fancybox", "roadtrip");
+      keyword[i].setAttribute("data-fancybox", "roadtrip");
     }
   }
 
